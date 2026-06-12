b37f9c63d4096f59bdc69caaf2ab9c8bc6153604
Ahmad Reza <100871767+ahmadrz1379@users.noreply.github.com>
2026-06-12 18:56:13 +0330

    🚀 feat: day 1 - mastering the fmt package

---
+++ 01-basics/01-fmt.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// استفاده از := برای تعریف و مقداردهی همزمان (خیلی Go-پسندتره!)
+	name := "ali"
+	age := 18
+	score := 48.6850
+
+	fmt.Println("--- Printing Variables ---")
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(score)
+
+	fmt.Println("\n--- Formatted Sentence ---")
+	fmt.Printf("user: %s | Age is: %d | Score: %.2f\n", name, age, score)
+
+	fmt.Println("\n--- Variable Types ---")
+	fmt.Printf("type of score: %T\n", score)
+	fmt.Printf("type of name: %T\n", name)
+	fmt.Printf("type of age: %T\n", age)
+}
